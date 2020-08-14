08b8384e28c772c446d511bf8dcf77dd8b0f619d
liuyingduo <A2711985388@163.com>
2020-08-14 12:08:50 +0800

    suricata chinese

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'liuyingduo'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
